e2cb48be7c19ed53bc0d6cbf468049f1875a514a
Ivan Čavlek <ivan.cavlek@gmail.com>
2016-10-07 23:02:55 +0200

    Added base entity for SEPS

---
+++ SEPS/Acme.Seps.Domain.Base.Test.Unit/Entity/SepeBaseEntityTests.cs
@@ -0,0 +1,78 @@
+﻿using Acme.Domain.Base.DomainService;
+using Acme.Seps.Domain.Base.Entity;
+using FluentAssertions;
+using Moq;
+using System;
+
+namespace Acme.Seps.Domain.Base.Test.Unit.Entity
+{
+    public class SepsBaseEntityTests
+    {
+        private readonly Mock<ITimeZone> _timeZone;
+        private readonly DateTimeOffset _repositoryTime;
+
+        public SepsBaseEntityTests()
+        {
+            _timeZone = new Mock<ITimeZone>();
+            _repositoryTime = DateTimeOffset.UtcNow;
+        }
+
+        public void ThrowsExceptionIfTimeZoneIsNotSet()
+        {
+            Action action = () => new DummySepsBaseEntity(null);
+
+            action.ShouldThrow<ArgumentNullException>();
+        }
+
+        public void SetsInitialPeriodUponCreation()
+        {
+            _timeZone.Setup(m => m.GetCurrentRepositoryDateTime()).Returns(_repositoryTime);
+
+            var result = new DummySepsBaseEntity(_timeZone.Object);
+
+            result.Period.ValidFrom.Should().Be(_repositoryTime);
+            result.Period.ValidTill.Should().NotHaveValue();
+        }
+
+        public void ArchivesTheEntity()
+        {
+            var repositoryTimePlusOneMonth = _repositoryTime.AddMonths(1);
+            _timeZone.SetupSequence(m => m.GetCurrentRepositoryDateTime())
+                .Returns(_repositoryTime)
+                .Returns(repositoryTimePlusOneMonth);
+
+            var result = new DummySepsBaseEntity(_timeZone.Object);
+            result.Archive();
+
+            result.Period.ValidFrom.Should().Be(_repositoryTime);
+            result.Period.ValidTill.Should().HaveValue();
+            result.Period.ValidTill.Should().Be(repositoryTimePlusOneMonth);
+        }
+
+        public void CanOnlyBeArchivedOnce()
+        {
+            var repositoryTimePlusOneMonth = _repositoryTime.AddMonths(1);
+            var repositoryTimePlusTwoMonth = _repositoryTime.AddMonths(2);
+            _timeZone.SetupSequence(m => m.GetCurrentRepositoryDateTime())
+                .Returns(_repositoryTime)
+                .Returns(repositoryTimePlusOneMonth)
+                .Returns(repositoryTimePlusTwoMonth);
+
+            var result = new DummySepsBaseEntity(_timeZone.Object);
+            result.Archive();
+            result.Archive();
+
+            result.Period.ValidFrom.Should().Be(_repositoryTime);
+            result.Period.ValidTill.Should().HaveValue();
+            result.Period.ValidTill.Should().NotBe(repositoryTimePlusTwoMonth);
+            result.Period.ValidTill.Should().Be(repositoryTimePlusOneMonth);
+        }
+    }
+
+    internal class DummySepsBaseEntity : SepsBaseEntity
+    {
+        public DummySepsBaseEntity(ITimeZone timeZone) : base(timeZone)
+        {
+        }
+    }
+}
---
+++ SEPS/Acme.Seps.Domain.Base/Entity/SepsBaseEntity.cs
@@ -0,0 +1,31 @@
+﻿using Acme.Domain.Base.DomainService;
+using Acme.Domain.Base.Entity;
+using Acme.Domain.Base.ValueType;
+using System;
+
+namespace Acme.Seps.Domain.Base.Entity
+{
+    public abstract class SepsBaseEntity : BaseEntity
+    {
+        private readonly ITimeZone _timeZone;
+
+        public Period Period { get; private set; }
+
+        protected SepsBaseEntity(ITimeZone timeZone)
+        {
+            if (timeZone == null)
+                throw new ArgumentNullException(nameof(timeZone));
+
+            _timeZone = timeZone;
+            Period = new Period(_timeZone.GetCurrentRepositoryDateTime());
+        }
+
+        public void Archive()
+        {
+            if (!IsArchived())
+                Period = Period.SetValidTill(_timeZone.GetCurrentRepositoryDateTime());
+        }
+
+        private bool IsArchived() => Period.ValidTill.HasValue;
+    }
+}

9e0904302687955de83ce709c41879ae148e98bc
Ivan Čavlek <ivan.cavlek@gmail.com>
2016-10-08 22:00:03 +0200

    Added SEPS base application service with logging event handler

---
+++ SEPS/Acme.Seps.Domain.Base/ApplicationService/SepsBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acme.Seps.Domain.Base.ApplicationService
+{
+    public abstract class SepsBaseService
+    {
+        public event EventHandler<string> UseCaseExecutionProcessing = delegate { };
+
+        protected void Log(string useCaseExecutionProcessingLog) =>
+            UseCaseExecutionProcessing(this, useCaseExecutionProcessingLog);
+    }
+}

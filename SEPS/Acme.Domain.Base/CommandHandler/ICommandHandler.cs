26b7c9b35835f0270819910ed9c5bbe28763ef52
Ivan Čavlek <I.Cavlek@douglas.de>
2019-01-02 17:05:45 +0100

    Added comments for sources

--- SEPS/Acme.Domain.Base/CommandHandler/ICommandHandler.cs
+++ SEPS/Acme.Domain.Base/CommandHandler/ICommandHandler.cs
@@ -1,5 +1,8 @@
 ﻿namespace Acme.Domain.Base.CommandHandler
 {
+    /// <summary>
+    /// <see href="https://www.cuttingedge.it/blogs/steven/pivot/entry.php?id=91">Command credits</see>
+    /// </summary>
     public interface ICommandHandler<TCommand>
     {
         void Handle(TCommand command);
--- SEPS/Acme.Domain.Base/QueryHandler/IQueryHandler.cs
+++ SEPS/Acme.Domain.Base/QueryHandler/IQueryHandler.cs
@@ -2,6 +2,9 @@
 
 namespace Acme.Domain.Base.QueryHandler
 {
+    /// <summary>
+    /// <see href="https://www.cuttingedge.it/blogs/steven/pivot/entry.php?id=92">Query credits</see>
+    /// </summary>
     public interface IQueryHandler<TQuery, TQueryResult> where TQuery : IQuery<TQueryResult>
     {
         TQueryResult Handle(TQuery query);
